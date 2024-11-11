5f6893bd24f2cc7ae25bbb23af1201169e8ab249
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-11-11 16:05:49 +0900

    update

---
+++ 07-interfaces/Driver.ts
@@ -0,0 +1,14 @@
+import { CricketCoach } from "./CricketCoach";
+import { GolfCoach } from "./GolfCoach";
+import { Coach } from "./Coach";
+
+let myCricketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+let theCoaches: Coach[] = [];
+
+theCoaches.push(myCricketCoach);
+theCoaches.push(myGolfCoach);
+
+for (let tempCoach of theCoaches) {
+  console.log(tempCoach.getDailyWorkout());
+}
